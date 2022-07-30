40d54ca4ef6d426a8975cebfcca73ae04baa61ba
GuanSzzz <847388139@qq.com>
2022-07-30 17:23:14 +0800

    项目基本初始化

---
+++ src/settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+// 设置标题
+  title: 'hrassa',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  // 侧边栏的Logo
+  sidebarLogo: true
+}
